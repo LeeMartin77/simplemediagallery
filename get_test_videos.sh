811001692ff77ff1137e2162b091a39b7247aaa1
Lee Martin <hello@leejohnmartin.co.uk>
2023-08-29 23:02:13 +0100

    added: crude/quick video insert

--- get_test_videos.sh
+++ get_test_videos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(cd example/video && curl -L -O http://ftp.halifax.rwth-aachen.de/blender/demo/movies/ToS/tears_of_steel_720p.mov)
+(cd example/video && curl -L -O http://ftp.halifax.rwth-aachen.de/blender/demo/movies/ToS/tears_of_steel_1080p.webm)
--- src/index.php
+++ src/index.php
@@ -8,10 +8,46 @@ $directory = __DIR__ . '/_media' . $root;
 
 // breadcrumb
 
+function isImageFile($filePath) {
+    $allowedMimeTypes = [
+        'image/jpeg',
+        'image/png',
+        'image/gif',
+        'image/bmp',
+        'image/webp',
+        // Add more image MIME types as needed
+    ];
+
+    $mime = mime_content_type($filePath);
+
+    return in_array($mime, $allowedMimeTypes);
+}
+
+function isEmbeddableVideo($filePath) {
+    $allowedMimeTypes = [
+        'video/mp4',
+        'video/webm',
+        'video/ogg',
+        'video/quicktime',
+    ];
+
+    $mime = mime_content_type($filePath);
+
+    return in_array($mime, $allowedMimeTypes);
+}
+
 if (is_file($directory)) {
     // minimal gallery view
     echo "is file";
     $raw_file = '/_media' . $root;
+    if (isImageFile($directory)) {
+        echo "<img src='$raw_file' />";
+    }
+    echo mime_content_type($directory);
+    if (isEmbeddableVideo($directory)) {
+        $video_type = mime_content_type($directory);
+        echo "<video><source src='$raw_file' type='$video_type'/>Unsupported Browser</video>";
+    }
     echo "<a href='$raw_file'>Full File</a>";
     
 } else if (is_dir($directory)) {
