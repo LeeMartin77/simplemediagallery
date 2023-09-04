1aecef3c176d7b8d9c1190be5cf7ca17e9aace98
Lee Martin <hello@leejohnmartin.co.uk>
2023-09-04 07:51:06 +0100

    update: Content Viewer

--- src/_includes/ContentViewer.php
+++ src/_includes/ContentViewer.php
@@ -23,15 +23,17 @@ function isEmbeddableVideo($filePath) {
 function renderContentViewer($root, $filepath) {
     // minimal gallery view
     $raw_file = '/_media' . $root;
+    echo "<div class='content'>";
     if (isImageFile($filepath)) {
         echo "<img src='$raw_file' />";
     }
-    echo mime_content_type($filepath);
     if (isEmbeddableVideo($filepath)) {
         $video_type = mime_content_type($filepath);
         echo "<video><source src='$raw_file' type='$video_type'/>Unsupported Browser</video>";
     }
+    echo mime_content_type($filepath);
     echo "<a href='$raw_file'>Full File</a>";
+    echo "</div>";
 }
 
 ?>
