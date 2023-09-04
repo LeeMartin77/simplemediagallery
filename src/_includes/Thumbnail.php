31f8c91de383e4d7f72cbd17c7c73e5992bffa22
Lee Martin <hello@leejohnmartin.co.uk>
2023-09-04 07:29:31 +0100

    update: Better thumb icons

--- src/_includes/Thumbnail.php
+++ src/_includes/Thumbnail.php
@@ -2,7 +2,10 @@
 function renderThumbnail($mediaroot, $file) {
   echo "<div class='thumbnail'>";
   $mediafile = $mediaroot . "/" . $file;
-  $link = $_SERVER['REQUEST_URI'] . $file;
+  $link = $_SERVER['REQUEST_URI'] . '/' . $file;
+  if ($_SERVER['REQUEST_URI'] === '/') {
+    $link = '/' . $file;
+  }
   echo "<a href='$link'><img src='/thumbnail.php?file=$link' /></a>";
   if (is_file($mediafile)) {
     echo "<a href='$link'>$file</a>";
--- src/thumbnail.php
+++ src/thumbnail.php
@@ -2,18 +2,16 @@
 
 
 function isImageFile($filePath) {
-  $allowedMimeTypes = [
-      'image/jpeg',
-      'image/png',
-      'image/gif',
-      'image/bmp',
-      'image/webp',
-      // Add more image MIME types as needed
-  ];
-
   $mime = mime_content_type($filePath);
 
-  return in_array($mime, $allowedMimeTypes);
+  return strpos($mime, 'image/') === 0;
+}
+
+
+function isVideoFile($filePath) {
+  $mime = mime_content_type($filePath);
+
+  return strpos($mime, 'video/') === 0;
 }
 
 // Set the target dimensions
@@ -63,22 +61,74 @@ if (isImageFile($sourceImagePath)) {
   // Clean up resources
   imagedestroy($sourceImage);
   imagedestroy($resizedImage);
-} else {
-  $image = imagecreate($targetWidth, $targetHeight);
-  
-  // Allocate the white color
-  $whiteColor = imagecolorallocate($image, 255, 255, 255);
-  
-  // Fill the image with white color
-  imagefill($image, 0, 0, $whiteColor);
-  
+} else if (isVideoFile($sourceImagePath)) {
+  $sourceImage = imagecreatefrompng(__DIR__ . '/static/play.png');
+
+  // Get the source image dimensions
+  $sourceWidth = imagesx($sourceImage);
+  $sourceHeight = imagesy($sourceImage);
+
+  // Calculate the aspect ratio of the source image
+  $aspectRatio = $sourceWidth / $sourceHeight;
+
+  // Calculate the dimensions to fit within the target area while maintaining aspect ratio
+  if ($aspectRatio > 1) {
+      $newWidth = $targetWidth;
+      $newHeight = $targetWidth / $aspectRatio;
+  } else {
+      $newHeight = $targetHeight;
+      $newWidth = $targetHeight * $aspectRatio;
+  }
+
+  // Create a new image with the calculated dimensions
+  $resizedImage = imagecreatetruecolor($newWidth, $newHeight);
+
+  // Resize and copy the source image to the new image
+  imagecopyresampled($resizedImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $sourceWidth, $sourceHeight);
+
   // Set the header to indicate it's a JPEG image
   header('Content-Type: image/jpeg');
-  
-  // Output the image as JPEG
-  imagejpeg($image);
-  
+
+  // Output the resized image as JPEG
+  imagejpeg($resizedImage);
+
   // Clean up resources
-  imagedestroy($image);
+  imagedestroy($sourceImage);
+  imagedestroy($resizedImage);
+} else {
+  // probably a directory
+  $sourceImage = imagecreatefrompng(__DIR__ . '/static/folder.png');
+
+  // Get the source image dimensions
+  $sourceWidth = imagesx($sourceImage);
+  $sourceHeight = imagesy($sourceImage);
+
+  // Calculate the aspect ratio of the source image
+  $aspectRatio = $sourceWidth / $sourceHeight;
+
+  // Calculate the dimensions to fit within the target area while maintaining aspect ratio
+  if ($aspectRatio > 1) {
+      $newWidth = $targetWidth;
+      $newHeight = $targetWidth / $aspectRatio;
+  } else {
+      $newHeight = $targetHeight;
+      $newWidth = $targetHeight * $aspectRatio;
+  }
+
+  // Create a new image with the calculated dimensions
+  $resizedImage = imagecreatetruecolor($newWidth, $newHeight);
+
+  // Resize and copy the source image to the new image
+  imagecopyresampled($resizedImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $sourceWidth, $sourceHeight);
+
+  // Set the header to indicate it's a JPEG image
+  header('Content-Type: image/jpeg');
+
+  // Output the resized image as JPEG
+  imagejpeg($resizedImage);
+
+  // Clean up resources
+  imagedestroy($sourceImage);
+  imagedestroy($resizedImage);
 }
 ?>
